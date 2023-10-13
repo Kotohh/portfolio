c7dadeed50f284e6d367446ffe6d6004fffe683a
Kotohh <babou12240@gmail.com>
2023-10-14 01:14:22 +0200

    Update script

--- scripts/color_changer.js
+++ scripts/color_changer.js
@@ -9,14 +9,14 @@ button.addEventListener('click', () => {
     if (isDarkMode) {
         body.style.backgroundColor = '#222';
         body.style.color = '#ffffff';
-        button.style.backgroundColor = '#ffe100';
+        button.style.backgroundColor = '#ffe10000';
         imgElement.src = "images/sun_icon.png";
         button.innerHTML = "";
         button.appendChild(imgElement);
     } else {
         body.style.backgroundColor = '#f0f0f0';
         body.style.color = '#000000';
-        button.style.backgroundColor = '#ffe100';
+        button.style.backgroundColor = '#ffe10000';
         button.style.color = '#000000';
         imgElement.src = "images/moon_icon.png";
         button.innerHTML = "";
