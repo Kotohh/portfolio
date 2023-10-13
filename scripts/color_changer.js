4bb678e39913c7cac2c509ec7f35bd6bd4aa5f81
Kotohh <babou12240@gmail.com>
2023-10-14 01:02:53 +0200

    Update for dark mode

--- scripts/color_changer.js
+++ scripts/color_changer.js
@@ -1,6 +1,8 @@
 const button = document.getElementById('color-change-button');
+const imgElement = document.createElement("img");
 const body = document.body;
 let isDarkMode = false;
+
 
 button.addEventListener('click', () => {
     isDarkMode = !isDarkMode;
@@ -8,12 +10,16 @@ button.addEventListener('click', () => {
         body.style.backgroundColor = '#222';
         body.style.color = '#ffffff';
         button.style.backgroundColor = '#ffe100';
-        button.textContent = 'White Mode';
+        imgElement.src = "images/sun_icon.png";
+        button.innerHTML = "";
+        button.appendChild(imgElement);
     } else {
         body.style.backgroundColor = '#f0f0f0';
         body.style.color = '#000000';
         button.style.backgroundColor = '#ffe100';
         button.style.color = '#000000';
-        button.textContent = 'Dark Mode';
+        imgElement.src = "images/moon_icon.png";
+        button.innerHTML = "";
+        button.appendChild(imgElement);
     }
 });
